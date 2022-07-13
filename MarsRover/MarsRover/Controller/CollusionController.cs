b7aeffbd7b284db0df3affd4e327d6a7a7d14d4b
Berkay Yazıcı <44025038+berkayyazici@users.noreply.github.com>
2022-07-13 13:28:31 +0300

    Add files via upload

---
+++ MarsRover/MarsRover/Controller/CollusionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsRover.Models;
+using MarsRover.Movements;
+
+namespace MarsRover.Controller
+{
+    internal class CollusionController
+    {
+        Obstacles obstacles = new Obstacles();
+
+        public void CollusionControl()
+        {
+            int z = 0; // rows
+            int y = 1; // columns
+
+            for (int i = 0; i < obstacles.obstacle.GetLength(0); i++)
+            {
+                if (Rover.x_coordinate == obstacles.obstacle[i, z])
+                {
+                    if (Rover.y_coordinate == obstacles.obstacle[i, y])
+                    {
+                        Rover.y_coordinate -= 1;
+                        Console.WriteLine("Rover's X Coordinate : {0}, Y Coordinate : {1}, Direction : {2}, STOPPED !",
+                        Rover.x_coordinate, Rover.y_coordinate, Rover.direction);
+                        break;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
